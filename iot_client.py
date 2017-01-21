ed501d0b73d882c7cb4d918f96b4c5b11b3de06f
Chialin Chou <chialin668@gmail.com>
2017-01-20 21:46:15 -0800

    clean up code

--- iot_client.py
+++ iot_client.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import sys
 import logging
@@ -8,50 +9,35 @@ host='a1arqmop0meczp.iot.us-west-2.amazonaws.com'
 rootCAPath='./ssl/root-CA.crt'
 certificatePath='./ssl/MyThing02.cert.pem'
 privateKeyPath='./ssl/MyThing02.private.key'
-useWebsocket=False
 
-# Configure logging
 logger = logging.getLogger("AWSIoTPythonSDK.core")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 streamHandler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 streamHandler.setFormatter(formatter)
 logger.addHandler(streamHandler)
 
 def callback(client, userdata, message):
-        print("Received a new message: ")
-        print(message.payload)
-        print("from topic: ")
-        print(message.topic)
-        print("--------------\n\n")
+    print("Received a new message: " + message.payload + " from topic: " + message.topic)
 
-# Init AWSIoTMQTTClient
 client = None
-if useWebsocket:
-        client = AWSIoTMQTTClient("basicPubSub", useWebsocket=True)
-        client.configureEndpoint(host, 443)
-        client.configureCredentials(rootCAPath)
-else:
-        client = AWSIoTMQTTClient("basicPubSub")
-        client.configureEndpoint(host, 8883)
-        client.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+client = AWSIoTMQTTClient("basicPubSub")
+client.configureEndpoint(host, 8883)
+client.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
 
-# AWSIoTMQTTClient connection configuration
 client.configureAutoReconnectBackoffTime(1, 32, 20)
 client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 client.configureDrainingFrequency(2)  # Draining: 2 Hz
-client.configureConnectDisconnectTimeout(10)  # 10 sec
-client.configureMQTTOperationTimeout(5)  # 5 sec
+client.configureConnectDisconnectTimeout(10)  
+client.configureMQTTOperationTimeout(5)  
 
-# Connect and subscribe to AWS IoT
 client.connect()
 client.subscribe("sdk/test/Python", 1, callback)
 time.sleep(2)
 
-# Publish to the same topic in a loop forever
 loopCount = 0
 while True:
-        client.publish("sdk/test/Python", "New Message " + str(loopCount), 1)
-        loopCount += 1
-        time.sleep(60)
+    client.publish("sdk/test/Python", "New Message " + str(loopCount), 1)
+    loopCount += 1
+    time.sleep(5)
 
